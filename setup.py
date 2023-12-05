09374c7e4041d208b77e931994b3d26ec779bd61
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2023-12-05 21:20:43 +0800

    v0.2.2 update pack-pay-uri and snapshots for safe version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mixinsdk",
-    version="0.2.1",
+    version="0.2.2",
     author="nodewee",
     author_email="nodewee@gmail.com",
     description="python sdk for mixin: https://github.com/nodewee/mixin-sdk-python",
